8e0b8d70dee3da8a5e5d8f68df897bdbb8d7b3ca
Michael <64235745+michaelhoang781@users.noreply.github.com>
2021-11-06 10:23:53 -0400

    Create UI shell

--- client/App.jsx
+++ client/App.jsx
@@ -1,23 +1,31 @@
-
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Track from './components/Track';
+import List from './components/List';
+import Detail from './components/Detail';
+import Wing from './public/images/sprint.png'; 
 import './stylesheets/styles.scss';
 
 const App = () => {
 
+  const [tracking, setTracking] = useState('');
+  
   return (
     <Router>
-      <div>
-        <div id="navbar">
-          <header>Hermes</header>
-          <nav>
-            <ul>
-              <li>I</li>
-              <li>am</li>
-              <li>a</li>
-              <li>navbar</li>
-            </ul>
-          </nav>
+      <div className="body-wrapper">
+        <div className="header-wrapper">
+          <div className="logo-wrapper">
+            <img className="wing-logo" src={Wing} />
+            <h1 className="app-header">hermes</h1>
+          </div>
+        <div className="log-out-wrapper">
+            <h3 className="log-out-button">Log Out</h3>
+          </div>
+        </div>
+          <Track />
+        <div className="card-container">
+          <List />
+          <Detail />
         </div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
