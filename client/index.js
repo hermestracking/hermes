bafa58e66f63f1ac3c36c2e48dd88211a4d7054b
michaelhoang781 <64235745+michaelhoang781@users.noreply.github.com>
2021-11-06 16:25:36 -0400

    Revert "Front end integration v2"

--- client/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-// import { BrowserRouter, Link } from 'react-router-dom';
-import App from './App';
-
-render(
-  <App />,
-  document.getElementById('app'),
-);
