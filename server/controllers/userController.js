f4f66a7f6d82b6a60abb2bff4d7b7a2aae156e14
Robert Howton <robert.f.howton@gmail.com>
2021-11-06 15:05:42 -0400

    implemented bcrypt hashing on user passwords

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,15 @@
-const model = require('../models/userModel.js');
+const model = require('../models/userModel');
 
 const userController = {};
 
 userController.addUser = (req, res, next) => {
   try {
-    const { name, address, email } = req.body;
-    model.User.create(
+    const { name, email, password, address } = req.body;
+    const newUser = new model.User(
       {
         name: name,
+        email: email,
+        password: password,
         address: {
           address1: address.address1,
           address2: address.address2,
@@ -15,9 +17,10 @@ userController.addUser = (req, res, next) => {
           state: address.state,
           zip: address.zip,
         },
-        email: email,
-      },
-      (err, user) => {
+      })
+      newUser.save((err, user) => {
+        console.log(user);
+        if (err) return next(err);
         res.locals.newUser = user;
         return next();
       }
@@ -32,10 +35,33 @@ userController.getUsers = (req, res, next) => {
     model.User.find({}, (err, users) => {
       res.locals.users = users;
       return next();
-    })
+    });
   } catch (err) {
     return next(err);
   }
-}
+};
+
+userController.findUser = (req, res, next) => {
+  const { email, password } = req.body
+  try {
+    model.User.findOne ({ email })
+      .then(user => {
+        user.validatePassword(user.password, password, (err, isAMatch) => {
+          if (err) return next(err);
+          // user.generateToken(user, (err, user) => {
+          //   if (err) return next(err);
+          //   res.cookie('hermes', user.token, {
+          //     maxAge: 1800000,
+          //     httpOnly: true
+          //   });
+
+          //   return next();
+          // });
+        });
+      });
+  } catch (err) {
+    return next(err);
+  };
+};
 
 module.exports = userController;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { useLayoutEffect } = require('react');
 
 const MONGO_URI =
   'mongodb+srv://hermes:h3rm3sh3rm3s@codesmith.jttpr.mongodb.net/hermes?retryWrites=true&w=majority';
 
-  mongoose
+mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -16,6 +19,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, minlength: 8 },
+  token: String,
   address: {
     address1: { type: String, required: true },
     address2: String,
@@ -23,9 +29,44 @@ const userSchema = new Schema({
     state: { type: String, required: true },
     zip: { type: Number, required: true },
   },
-  email: { type: String, required: true },
   trackingNumbers: { type: Number },
 });
+
+userSchema.pre('save', function(next) {
+  console.log('pre fired!')
+  let user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, (err, hashedPassword) => {
+        if (err) return next(err);
+        user.password = hashedPassword;
+        return next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.validatePassword = (hashed, password, callback) => {
+  bcrypt.compare(password, hashed, (err, res) => {
+    if (err) callback(err);
+    callback(null, res);
+  });
+};
+
+userSchema.methods.generateToken = (user, callback) => {
+  // const token = jwt.sign({id: user._id, 'jwtsecret'});
+
+  // user.token = token;
+
+  user.save()
+    .then(user => {
+      callback(null, user);
+    })
+    .catch(err => callback(err));
+};
 
 const User = mongoose.model('user', userSchema);
 
