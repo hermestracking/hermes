40e6ddf5204818c8ce28613efa0bd7d27274de55
Robert Howton <robert.f.howton@gmail.com>
2021-11-06 11:12:55 -0400

    added component files

---
+++ client/components/Detail.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Detail = () => {
+    return(
+        <React.Fragment>
+            <div className="detail-view-container">
+                <div className="detail-header-wrapper">
+                    <h2 className="detail-header">Details:</h2>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+};
+
+export default Detail; 
