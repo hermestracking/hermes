96bb19e37a374b397e3695527cabc69ce5ed844b
Owen Eldridge <oweneldridge7@gmail.com>
2021-11-08 10:19:57 -0500

    progress on calendar

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Track from './components/Track';
-import List from './components/List';
-import Detail from './components/Detail';
-import Wing from './public/images/sprint.png'; 
-import './stylesheets/styles.scss';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Track from "./components/Track";
+import List from "./components/List";
+import Detail from "./components/Detail";
+import Wing from "./public/images/sprint.png";
+import "./stylesheets/styles.scss";
+import Calendar from "./components/Calendar";
 
 const App = () => {
+  const [tracking, setTracking] = useState("");
 
-  const [tracking, setTracking] = useState('');
-  
   return (
     <Router>
       <div className="body-wrapper">
@@ -18,28 +18,28 @@ const App = () => {
             <img className="wing-logo" src={Wing} />
             <h1 className="app-header">hermes</h1>
           </div>
-        <div className="log-out-wrapper">
+          <div className="log-out-wrapper">
             <h3 className="log-out-button">Log Out</h3>
           </div>
         </div>
-          <Track tracking={tracking} setTracking={setTracking} />
+        <Track tracking={tracking} setTracking={setTracking} />
         <div className="card-container">
           <List />
           <Detail />
+        </div>
+        <div className="calendar-container">
+          <Calendar />
         </div>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           {/* <Route path="/newPath">
           </Route> */}
-          <Route path="/">
-          </Route>
+          <Route path="/"></Route>
         </Switch>
       </div>
     </Router>
   );
 };
-
-
 
 export default App;
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MONGO_URI =
-  'mongodb+srv://hermes:h3rm3sh3rm3s@codesmith.jttpr.mongodb.net/hermes?retryWrites=true&w=majority';
+  "mongodb+srv://hermes:h3rm3sh3rm3s@codesmith.jttpr.mongodb.net/hermes?retryWrites=true&w=majority";
 
-  mongoose
+mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'hermes',
+    dbName: "hermes",
   })
-  .then(() => console.log('Connected to Mongo DB.'))
+  .then(() => console.log("Connected to Mongo DB."))
   .catch((err) => console.log(err));
 
 const Schema = mongoose.Schema;
@@ -27,6 +27,6 @@ const userSchema = new Schema({
   trackingNumbers: { type: Number },
 });
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = { User };
