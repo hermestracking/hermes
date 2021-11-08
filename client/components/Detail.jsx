c8f374d09d5baaad629e289ac9078b9eb9fc10de
bugarcic <baronvie@DESKTOP-B4L6IGS.localdomain>
2021-11-08 16:22:22 -0500

    status-add

--- client/components/Detail.jsx
+++ client/components/Detail.jsx
@@ -21,12 +21,10 @@ const Detail = (props) => {
                 <div className="detail-body">
                    <div className="tracking-number"> {!props.shipments.filter(orders => orders.trackingNumber === props.selectedItem.trackingNumber).length ? null : 'Tracking Number: ' + props.selectedItem.trackingNumber } </div>
                    <div className="expected-delivery">{!props.shipments.filter(orders => orders.deliveryDate === props.selectedItem.deliveryDate).length ? null : 'Expected Delivery: ' + moment(props.selectedItem.deliveryDate).format('MMMM do YYYY')}</div>
-
-                    {Object.keys(props.selectedItem).length > 0 && <img className="google-map" src={`https://maps.googleapis.com/maps/api/staticmap?center=${city},${state}&zoom=11&size=1000x1000&markers=size:small%7Ccolor:blue%7C${city},${state}&key=AIzaSyBJxF1BhB_ITohgRDtOc2AUErTYRcYkEjU`}></img>}
-
-
+                   <div className="expected-delivery"> {!props.shipments.filter(orders => orders.trackingNumber === props.selectedItem.trackingNumber).length ? null : 'Most Recent Update: ' + props.selectedItem.recentActivity[0].status.description } </div>
+                    {Object.keys(props.selectedItem).length > 0 && props.shipments.filter(orders => orders.trackingNumber === props.selectedItem.trackingNumber).length > 0 && <img className="google-map" src={`https://maps.googleapis.com/maps/api/staticmap?center=${city},${state}&zoom=11&size=1000x1000&markers=size:small%7Ccolor:blue%7C${city},${state}&key=AIzaSyBJxF1BhB_ITohgRDtOc2AUErTYRcYkEjU`}></img>}
+                    
                 </div>
-                
             </div>
         </React.Fragment>
     )
